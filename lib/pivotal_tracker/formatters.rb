cf26ce90f99181c4cb81256a7fd431807f931c7d
Josh Knowles <josh@weplay.com>
2008-10-31 14:00:26 -0400

    Only require Prawn if using the PDF formatter

--- lib/pivotal_tracker/formatters.rb
+++ lib/pivotal_tracker/formatters.rb
@@ -1,12 +1,26 @@
 module PivotalTracker::Formatters
   def self.formatter(options)
-    if options[:formatter] == "pdf"
-      PDFFormatter.new
-    else
-      ConsoleFormatter.new
-    end
+    @formatter ||= begin
+                    formatter   = options[:formatter]
+                    initializer = "initialize_#{formatter}_formatter"
+
+                    if self.respond_to?(initializer)
+                      self.send(initializer)
+                    else
+                      raise "Unknown Formatter: #{formatter}"
+                    end
+                   end
+  end
+
+private
+
+  def self.initialize_console_formatter
+    require "pivotal_tracker/formatters/console_formatter"
+    ConsoleFormatter.new
+  end
+
+  def self.initialize_pdf_formatter
+    require "pivotal_tracker/formatters/pdf_formatter"
+    PDFFormatter.new
   end
 end
-
-require "pivotal_tracker/formatters/console_formatter"
-require "pivotal_tracker/formatters/pdf_formatter"
